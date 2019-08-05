1676d1d87a0c5fe95416e762b8e7a4c28cd6e8e8
LioStein <liucx0112@mails.jlu.edu.cn>
2019-08-05 16:02:54 +0800

    ex 43：class heirachy

---
+++ ex43.py
@@ -0,0 +1,5 @@
+# write or draw about the problem
+# extract key concepts from 1 and research them
+# create a class hierarchy and object map for the concepts
+# code the classes and a test to run them
+# repeat and refine
